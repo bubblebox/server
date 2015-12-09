6d9cd3d20650e2a9677bb595d07cece69c43f1b6
Ariejan de Vroom <ariejan@ariejan.net>
2015-12-09 09:56:38 +0100

    Update Mirage config

--- client/app/mirage/config.js
+++ client/app/mirage/config.js
@@ -7,7 +7,7 @@ export default function() {
 
     Note: these only affect routes defined *after* them!
   */
-  this.urlPrefix = 'http://localhost:8042';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  this.urlPrefix = 'http://api.127.0.0.1.xip.io:8042';    // make this `http://localhost:8080`, for example, if your API is on a different server
   this.namespace = 'api/v1';    // make this `api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
