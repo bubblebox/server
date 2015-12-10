33279b96bbcf15d795a855d1ec41488bc247c500
Kai-Arne Reiter <git@mirrorspock.com>
2015-12-10 19:14:00 +0100

    Cherry picked item.content -> item.payload renaming

--- client/app/mirage/fixtures/items.js
+++ client/app/mirage/fixtures/items.js
@@ -1,21 +1,21 @@
 export default [
   {
     "code": "url1",
-    "content": "http://example.com",
+    "payload": "http://example.com",
     "type": "url",
     "created": "2015-12-10 19:19",
     "views": 1234
   },
   {
     "code": "url2",
-    "content": "http://example.org",
+    "payload": "http://example.org",
     "type": "url",
     "created": "2015-12-04 19:19",
     "views": 234
   },
   {
     "code": "txt1",
-    "content": "Lorem ipsum",
+    "payload": "Lorem ipsum",
     "type": "txt",
     "created": "2015-01-01 19:19",
     "views": 34
